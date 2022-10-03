c9c7c738f461f8c0f9f1a86f3135ef05abfdf568
Rahat Islam Akash <ami@pop-os.localdomain>
2022-10-03 19:20:56 +0600

    slices

--- print.go
+++ print.go
@@ -1,20 +1,35 @@
 package main
 
-import "log"
+import (
+	"log"
+	// f "first/deleteit"
+)
 
 /*
 *in go we use slices is more like  array
  */
 
- type Use struct{
+type Use struct {
 	FirstName string
 	LastName  string
- }
+}
+
 func main() {
 	var mySlice []string
 	var sliceNumber []int
 
-	
+	// struct type slice
+
+	var userName []Use
+
+	use := Use{
+		FirstName: "Asad",
+		LastName:  "abaro Asad",
+	}
+
+	userName = append(userName, use)
+
+	log.Println(userName)
 
 	mySlice = append(mySlice, "Rahat")
 	mySlice = append(mySlice, "Akash")
